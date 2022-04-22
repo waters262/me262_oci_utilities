97140075342066e5c8e113e8004721af7aee1d7c
waters262 <waters262@gmail.com>
2022-04-22 18:44:34 +0000

    Added testing script

---
+++ testing/testAuthClient.py
@@ -0,0 +1,65 @@
+import oci
+import sys
+# appending the directory for modules
+# in the sys.path list
+sys.path.append('../pyUtils/')       
+ 
+# now we can import mod
+from auth_client  import *
+
+if __name__ == "__main__":
+
+    print ('Number of arguments:', len(sys.argv), 'arguments.')
+    args = sys.argv
+    targetTenant = args[1]
+
+
+    try:
+        tenancyProfile = oci.config.from_file()[targetTenant]
+        tenancyConfig = oci.config.from_file(profile_name=tenancyProfile)
+    except:
+        print(tenancyProfile + ' Profile not found in config file.  Using Default Profile')
+        tenancyProfile = 'DEFAULT'
+        tenancyConfig = oci.config.from_file()
+
+    try:
+        if tenancyConfig['auth_type'] == 'security_token' :
+            config = ' --profile ' + tenancyConfig['auth_profile'] +  ' --auth security_token'
+        else:
+            config = ' --profile ' + tenancyConfig['auth_profile'] 
+        print('CLI Auth config string is: ', config)
+    except:
+        config = ' --profile ' + tenancyProfile
+        print('CLI config string is: ', config)
+    #     print("Auth Profile"   ,tenancyConfig['auth_profile']) 
+
+    print("Authentication Type is: ", tenancyConfig['auth_type'] )
+
+    object_storage_client = get_client( client_type='ObjectStorageClient',    tenancyConfig=tenancyConfig)
+    get_namespace_response = object_storage_client.get_namespace()
+    print("\nChecking Authentication ....")
+    print("Namespace is: ",  get_namespace_response.data)
+
+
+    client = get_client( client_type='IdentityClient',    tenancyConfig=tenancyConfig)
+    compts = client.list_compartments(compartment_id=tenancyConfig['tenancy']\
+                                    ,compartment_id_in_subtree=True)
+
+    network = get_client(client_type='VirtualNetworkClient',    tenancyConfig=tenancyConfig)
+    # network = oci.core.VirtualNetworkClient({'region': ssoRegion}, signer=signer)
+
+    print("Network var type: ", type(network))
+    print(network)
+
+
+
+
+
+#    signer =  oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
+
+#    healthchecks_client = oci.healthchecks.HealthChecksClient({}, signer=signer)
+
+#    apigateway_client = oci.apigateway.GatewayClient({}, signer=signer)
+    print('Running on Tenant: ' + tenancyProfile )
+
+
